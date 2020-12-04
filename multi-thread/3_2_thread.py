799656a4d556fb1d8ad7e2679a95f8edbfa2b46c
NULLPE <1989leosun@gmail.com>
2020-12-04 23:36:21 +0900

    multi thread and multi process

---
+++ multi-thread/3_2_thread.py
@@ -0,0 +1,42 @@
+import time
+import tkinter
+import tkinter.messagebox
+from threading import Thread
+
+
+def main():
+
+    # 将消耗时间的任务放到单独的线程中
+    class DownloadTaskHandler(Thread):
+
+        def run(self):
+            time.sleep(10)
+            tkinter.messagebox.showinfo('info', 'Download.')
+
+
+    def download():
+        button1.config(state=tkinter.DISABLED)
+        DownloadTaskHandler(daemon=True).start()
+
+
+    def show_about():
+        tkinter.messagebox.showinfo('about', 'author: nobody')
+            
+    top = tkinter.Tk()
+
+    top.title('single thread')
+    top.geometry('200x150')
+    top.wm_attributes('-topmost', True)
+
+    panel = tkinter.Frame(top)
+    button1 = tkinter.Button(panel, text='download', command=download)
+    button1.pack(side='left')
+    button2 = tkinter.Button(panel, text='about', command=show_about)
+    button2.pack(side='right')
+    panel.pack(side='bottom')
+
+    tkinter.mainloop()
+
+
+if __name__ == "__main__":
+    main()
